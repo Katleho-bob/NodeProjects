a8ddbec34499ba2a962b93d5bab4501b9a7200ab
Katleho-bob <63137812+Katleho-bob@users.noreply.github.com>
2021-06-03 11:53:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-/*var logger = require('./logger');
-
-//console.log(logger);
-logger.log("is this you?");*/
-//====================================================================================
-/*
-const path = require('path');
-
-var pathObject = path.parse(__filename);
-
-console.log(pathObject);*/
-//=======================================================================================
-/*
-const os = require('os');
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-// console.log('Total Memory: ' + os.totalMemory);
-// console.log('Free Memory: '+ os.freeMemory);
-//template string
-//ES6 / ES2021
-console.log(`Total Memory: ${totalMemory}`)
-console.log(`Free  Memory: ${freeMemory}`)
-*/
-//================================================
-/*
-const fs = require('fs');
-
-// const files = fs.readdirSync('./');
-// console.log(files); 
-
- fs.readdir('./', function(err,files) {
-     if(err) console.log('Error', err);
-     else console.log('Result',files);
- });
-*/
-//=================Events=======Module==================================
+//================Events=======Module==================================
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
